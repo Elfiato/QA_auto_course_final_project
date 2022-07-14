fb893ee09781e22d8717f8c6ce7b735802ec4191
Elfiato <doker912@gmail.com>
2022-07-14 21:37:56 +0500

    create login_page test for registration and login form

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators as LP
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert 'login' in self.browser.current_url, 'URL логина не содержит подстроки \'login\''
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LP.LOGIN_EMAIL), "Нет формы ввода почты при авторизации"
+        assert self.is_element_present(*LP.LOGIN_PASSWORD), "Нет формы ввода пароя при авторизации"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LP.REGISTRATION_EMAIL), "Нет формы ввода почты при регистрации"
+        assert self.is_element_present(*LP.REGISTRATION_PASSWORD), "Нет формы ввода первого пароя при регистрации"
+        assert self.is_element_present(
+            *LP.CONFIRM_REGISTRATION_PASSWORD), "Нет формы ввода второго пароя при регистрации"
