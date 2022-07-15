01959cf1cb53b92ec1a3f2886a2078783ab66302
Elfiato <doker912@gmail.com>
2022-07-15 16:39:49 +0500

    add is_not_element_present and is_disappeared to base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,6 @@
-from selenium.common.exceptions import NoAlertPresentException, NoSuchElementException, TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
-
-import math
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class BasePage:
@@ -17,7 +15,24 @@ class BasePage:
     def is_element_present(self, how, what):
         try:
             WebDriverWait(self.browser, 5).until(
-                EC.visibility_of_element_located((how, what)))
+                EC.presence_of_element_located((how, what)))
         except (NoSuchElementException, TimeoutException):
             return False
+        return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
         return True
